67242c95de4758b2454fa8b7f7eee702eb6deca7
tanabesan <152053837+tanabesan@users.noreply.github.com>
2023-12-14 08:49:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-function countp(){
-  var thisCount = $("#count").html();
-      thisCount = Number(thisCount);
-      thisCount = thisCount + 1;
-  $("#count").html(thisCount);
+
+let counter, btnAdd;
+let n;
+ 
+function addCount(){
+    n++;
+    counter.innerHTML = n;
 }
+ 
+window.addEventListener("load", ()=>{
+    // 起動時の処理
+    counter = document.getElementById("counter");
+    btnAdd = document.getElementById("btnAdd");
+    n = 0;
+ 
+    btnAdd.addEventListener("click", addCount);
+});
 
 alert('適応されてます！');
